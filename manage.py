d4b4b596258ecca012a96717c9c89f83f35160b9
Tyler de Graaff <tyler@labela.nl>
2019-07-04 14:52:47 +0200

    Add tests

--- manage.py
+++ manage.py
@@ -31,7 +31,7 @@ def test():
     """Runs the uit tests."""
     tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccesful():
+    if result.wasSuccessful():
         return 0
     return 1
 
